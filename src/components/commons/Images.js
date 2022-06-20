f99bc7a4cfeac0599f3c3f8feeaaa804f37b0e44
kramlj00 <kramlj00@fesb.hr>
2022-06-20 20:07:41 +0200

    Added LightBox component

--- src/components/commons/Images.js
+++ src/components/commons/Images.js
@@ -1,17 +1,61 @@
-import React from 'react'
+import React, { useState } from "react";
 import styled from "styled-components";
+import LightBox from "react-awesome-lightbox";
 
-const Images = ({imageList}) => {
+const Images = ({ imageList }) => {
+  const [showLight, setShowLight] = useState(null);
+
+  const showLightBox = (index) => {
+    setShowLight({
+      startIndex: index
+    });
+  };
+
+  const hideLightBox = () => {
+    setShowLight(null);
+  };
   return (
-    <ImagesContainer>
-      {imageList.map((image, index) => {
-        return <Image src={image} alt={index} key={index} />;
-      })}
-    </ImagesContainer>
-  )
-}
+    <>
+      <ImagesContainer>
+      {imageList.map((image, index) => (
+        <Image
+          src={image.src}
+          alt={image.title}
+          onClick={() => showLightBox(index)}
+        />
+      ))}
+      </ImagesContainer>
+      {showLight && (
+        <LightBox
+          images={imageList.map((img) => ({ url: img.src, title: img.caption }))}
+          startIndex={showLight.startIndex}
+          onClose={hideLightBox}
+          allowRotate={false}
+          showTitle={false}
+          buttonAlign="center"
+        />
+      )}
+    </>
+  );
+};
 
-export default Images
+export default Images;
+
+const Image = styled.img`
+  width: 350px;
+  height: 200px;
+  object-fit: cover;
+  cursor: pointer;
+
+  &:hover {
+    transform: scale(1.03);
+    transition: all 0.4s ease-out;
+  }
+
+  ${({ theme }) => `
+    margin: ${theme.spacing.small};
+  `}
+`;
 
 const ImagesContainer = styled.div`
   display: flex;
@@ -19,18 +63,8 @@ const ImagesContainer = styled.div`
   flex: 1;
   justify-content: space-around;
   z-index: -1;
-  
+
   ${({ theme }) => `
     margin-top: ${theme.spacing.larger};
-  `}
-`;
-
-const Image = styled.img`
-  width: 350px;
-  height: 200px;
-  object-fit: cover;
-
-  ${({ theme }) => `
-    margin: ${theme.spacing.small};
   `}
 `;
--- src/pages/roomsPage.js
+++ src/pages/roomsPage.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
 import Images from "../components/commons/Images";
 import TitleImage from "../components/commons/TitleImage";
+import LightBox from "react-awesome-lightbox";
+import "react-awesome-lightbox/build/style.css";
 
 const RoomsPage = () => {
-    const roomsImages = [
-        "/images/room-image.png",
-        "/images/room-image.png",
-        "/images/room-image.png",
-        "/images/room-image.png",
-        "/images/room-image.png",
-        "/images/room-image.png",
-        "/images/room-image.png",
-      ];
+  // const [isOpen, setIsOpen] = useState(true);
+  const roomsImages = [
+    {src: "/images/exterior-image.png", caption: "Image 1"},
+    {src: "/images/home-image.png", caption: "Image 2"},
+    {src: "/images/room-image.png", caption: "Image 3"},
+    {src: "/images/exterior-image.png", caption: "Image 4"},
+    {src: "/images/home-image.png", caption: "Image 5"},
+    {src: "/images/exterior-image.png", caption: "Image 6"},
+    {src: "/images/home-image.png", caption: "Image 7"},
+    {src: "/images/room-image.png", caption: "Image 8"},
+  ];
   return (
     <>
       <TitleImage
